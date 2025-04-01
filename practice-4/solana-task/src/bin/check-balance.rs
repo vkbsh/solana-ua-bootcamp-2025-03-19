f22d241dda6b229c669a1e3a429c7cd471707f2a
vkbsh <vitaliykibish@gmail.com>
2025-04-01 10:20:57 +0200

    fix: read key from .env

--- practice-4/solana-task/src/bin/check-balance.rs
+++ practice-4/solana-task/src/bin/check-balance.rs
@@ -1,6 +1,6 @@
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::{commitment_config::CommitmentConfig, native_token::LAMPORTS_PER_SOL, pubkey};
-
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -9,7 +9,7 @@ async fn main() -> anyhow::Result<()> {
         CommitmentConfig::confirmed(),
     );
 
-    let wallet = pubkey!("kbshAfYTZfF95J3bqsy34m4F4Kac1EHDPYBXsWt8YQX");
+    let wallet = std::env::var("PUBLIC_KEY").expect("PUBLIC_KEY must be set.");
     let balance = client.get_balance(&wallet).await?;
 
     println!("{} SOL", balance / LAMPORTS_PER_SOL);
